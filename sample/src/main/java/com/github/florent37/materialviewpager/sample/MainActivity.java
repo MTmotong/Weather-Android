818c5846aa8bde2e9d2f4622bb734d84c5a69b15
MTmotong <dzymt6627@163.com>
2017-07-20 16:56:23 +0800

    fragment

--- sample/src/main/java/com/github/florent37/materialviewpager/sample/MainActivity.java
+++ sample/src/main/java/com/github/florent37/materialviewpager/sample/MainActivity.java
@@ -3,59 +3,49 @@ package com.github.florent37.materialviewpager.sample;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.crashlytics.android.Crashlytics;
 import com.github.florent37.materialviewpager.MaterialViewPager;
 import com.github.florent37.materialviewpager.header.HeaderDesign;
+import com.github.florent37.materialviewpager.sample.Activity.DrawerActivity;
+import com.github.florent37.materialviewpager.sample.fragment.ChooseAreaFragment;
 import com.github.florent37.materialviewpager.sample.fragment.RecyclerViewFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.fabric.sdk.android.Fabric;
+
 
 public class MainActivity extends DrawerActivity {
 
     @BindView(R.id.materialViewPager)
     MaterialViewPager mViewPager;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawLayout;
 
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private ChooseAreaFragment chooseAreaFragment;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("");
         ButterKnife.bind(this);
+        initFragment();
 
         final Toolbar toolbar = mViewPager.getToolbar();
         if (toolbar != null) {
             setSupportActionBar(toolbar);
         }
-
-//        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-//            @Override
-//            public boolean onMenuItemClick(MenuItem item) {
-//                switch (item.getItemId()) {
-//                    case R.id.action_settings:
-//                        Toast.makeText(MainActivity.this, "action_settings", Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case R.id.action_share:
-//                        Toast.makeText(MainActivity.this, "action_share", Toast.LENGTH_SHORT).show();
-//                        break;
-//                    default:
-//                        break;
-//                }
-//                return true;
-//            }
-//        });
 
         mViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
 
@@ -142,8 +132,38 @@ public class MainActivity extends DrawerActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
+                fragmentTransaction = fragmentManager.beginTransaction();
+                hideAllFragment(fragmentTransaction);
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        mDrawLayout.openDrawer(GravityCompat.START);
+                        break;
+                    case R.id.nav_addcity:
+                        if (chooseAreaFragment == null) {
+                            chooseAreaFragment = new ChooseAreaFragment();
+                            fragmentTransaction.add(R.id.choose_area_fragment, chooseAreaFragment);
+                        }
+                        Toast.makeText(MainActivity.this, "dianle ", Toast.LENGTH_SHORT).show();
+                }
+                fragmentTransaction.commit();
+//                mDrawLayout.closeDrawers();
                 return true;
             }
         });
+        
     }
-}
+    private void initFragment() {
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        hideAllFragment(fragmentTransaction);
+    }
+    private void hideAllFragment(FragmentTransaction fragmentTransaction) {
+//        if(weathPage != null) {
+//            fragmentTransaction.hide(weatherPage);
+//        }
+    }
+    @Override
+    public boolean OnNavigationItemSelected(MenuItem item) {
+        return false;
+    }
+ }
